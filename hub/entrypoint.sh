c12ce15148224123fb602c0a51be9b09f5f85463
Artem Eroshenko <eroshenkoam@qameta.io>
2017-02-14 22:31:15 +0300

    Update entrypoint.sh

--- hub/entrypoint.sh
+++ hub/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "Starting selenoid..."
-selenoid -conf /etc/selenoid/browsers.json -limit ${MAX_INSTANCES} -docker-api ${DOCKER_API}
+selenoid -conf /etc/selenoid/browsers.json -limit ${MAX_INSTANCES}
